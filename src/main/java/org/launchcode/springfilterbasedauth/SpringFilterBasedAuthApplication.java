10b1208eb86ea1cd39e31d5879b47b5df394e39c
Chris Bay <chris@chrisbay.net>
2017-07-26 09:57:19 -0500

    Initial commit. Basic project structure.

---
+++ src/main/java/org/launchcode/springfilterbasedauth/SpringFilterBasedAuthApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.springfilterbasedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilterBasedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilterBasedAuthApplication.class, args);
+	}
+}
